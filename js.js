fba500a1da3e4547735d7c010599ac4d74ff9acd
Victor Paranhos <victor_paranhos@hotmail.com>
2022-03-09 14:50:53 -0300

    Update js.js

--- js.js
+++ js.js
@@ -13,8 +13,7 @@ const ulz = document.getElementById("listademenu")
 function ShowSocialMedia() {
     for(let li of ulz.children){
         const social = li.getAttribute('class');
-        const link = li.children[0]
-        link.href = `https://${social}.com/${MeuLink[social]}`
+        li.children[0].href = `https://${social}.com/${MeuLink[social]}`
               
     }
     
